e983e0e2d83cf374ac7695f10a02bb8f6acfcf68
Lai Kah Hoe <kahlai@redhat.com>
2024-04-23 21:12:40 +0800

    clean up

--- code/linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 ./get-pip.py
-mldir=~/tmp
-sudo dnf install cronie cronie-anacron
-cd stock-sentiment-analysis
-sudo TMPDIR=~/tmp/ python3 -m pip install -r requirements.txt --no-cache-dir
-cd code
-sudo python3 app.py
